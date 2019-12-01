mail-in-a-box/mailinabox

commit 802e7a1f4d315a1bd10c1dfae259f6779f940ed0

Copy systemd service files before linking to avoid issue with order of mounting filesystems (#1688)

setup/management.sh
@@ -93,7 +93,8 @@ source $venv/bin/activate
 exec python `pwd`/management/daemon.py
 EOF
 chmod +x $inst_dir/start
-hide_output systemctl link -f conf/mailinabox.service
+cp -f conf/mailinabox.service /lib/systemd/system/mailinabox.service
+hide_output systemctl link -f /lib/systemd/system/mailinabox.service
 hide_output systemctl daemon-reload
 hide_output systemctl enable mailinabox.service
 

setup/munin.sh
@@ -64,7 +64,8 @@ mkdir -p /var/lib/munin-node/plugin-state/
 # Create a systemd service for munin.
 ln -sf $(pwd)/management/munin_start.sh /usr/local/lib/mailinabox/munin_start.sh
 chmod 0744 /usr/local/lib/mailinabox/munin_start.sh
-hide_output systemctl link -f conf/munin.service
+cp -f conf/munin.service /lib/systemd/system/munin.service
+hide_output systemctl link -f /lib/systemd/system/munin.service
 hide_output systemctl daemon-reload
 hide_output systemctl unmask munin.service
 hide_output systemctl enable munin.service

emmanuelvargas/power-mailinabox

commit 1fc196948634219f3df121232e8299bfdd08bed2

Fix upgrade issue broken by 802e7a1f4d315a1bd10c1dfae259f6779f940ed0

setup/management.sh
@@ -93,7 +93,7 @@ source $venv/bin/activate
 exec python `pwd`/management/daemon.py
 EOF
 chmod +x $inst_dir/start
-cp -f conf/mailinabox.service /lib/systemd/system/mailinabox.service
+cp --remove-destination conf/mailinabox.service /lib/systemd/system/mailinabox.service # target was previously a symlink so remove it first
 hide_output systemctl link -f /lib/systemd/system/mailinabox.service
 hide_output systemctl daemon-reload
 hide_output systemctl enable mailinabox.service

setup/munin.sh
@@ -64,7 +64,7 @@ mkdir -p /var/lib/munin-node/plugin-state/
 # Create a systemd service for munin.
 ln -sf $(pwd)/management/munin_start.sh /usr/local/lib/mailinabox/munin_start.sh
 chmod 0744 /usr/local/lib/mailinabox/munin_start.sh
-cp -f conf/munin.service /lib/systemd/system/munin.service
+cp --remove-destination conf/munin.service /lib/systemd/system/munin.service # target was previously a symlink so remove first
 hide_output systemctl link -f /lib/systemd/system/munin.service
 hide_output systemctl daemon-reload
 hide_output systemctl unmask munin.service